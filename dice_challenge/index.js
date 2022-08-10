28513e0b14dba6749c0e82e041215ce83ef749f0
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-08-11 04:56:10 +0530

    completed javascript dice challenge

---
+++ dice_challenge/index.js
@@ -0,0 +1,5 @@
+
+// if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+//     console.info( "This page is reloaded" );
+//     document.querySelectorAll(".dice")[0].setAttribute("src", "./images/dice1.png");
+// }
