7f48d32c0f794cf4fa2a576a0991fe0b5c6b31de
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-08-15 02:25:31 +0530

    EJS-Project Finished

--- EJS-Project/app.js
+++ EJS-Project/app.js
@@ -1,1 +1,33 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
+const app = express()
 
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+const item = ["Prep", "Cook", "Eat"]
+const workItems = []
+
+app.get("/", (req, res)=>{    
+    let day = date.getDate()
+    res.render("index", {listTitle: day, newListItem: item})
+})
+
+app.post("/", (req, res) => {
+    item.push(req.body.newItem)
+
+    res.redirect("/")
+})
+
+app.get("/work", (req, res) => {
+    res.render("index.ejs", {listTitle: "Work List", newListItem: workItems})
+})
+app.post("/work", (req, res) => {
+    workItems.push(req.body.newItem)
+    res.redirect("/work")
+})
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+})
